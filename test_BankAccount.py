a3683e6f9d54d529b2cb30ec40984139fb3760e9
preethirao-smu <preethirao@smu.edu.sg>
2021-07-19 11:09:09 +0800

    test-commit 1

---
+++ test_BankAccount.py
@@ -0,0 +1,26 @@
+"""
+TDD using pytest
+test file for BankAccount class
+"""
+# Below import statement implies - BankAccount class is being imported from file Bank_Account
+from Bank_Account import BankAccount
+
+# function to test the deposit() func of BankAccount
+# create a BankAccount Object
+# Call the deposit() func
+# assert check the balance if it contains correct balance after deposit()
+def test_deposit():
+    account = BankAccount(124124,100)
+    assert account.getAcctNum() == 124124
+    account.deposit(100)
+    assert account.getBalance() == 200
+
+# function to test the withdraw() func of BankAccount
+# create a BankAccount Object
+# Call the withdraw() func
+# assert that balance is correct after withdraw()
+def test_withdraw():
+    account = BankAccount(2345667, 200)
+    assert account.getAcctNum() == 2345667
+    account.withdraw(50)
+    assert account.getBalance() == 150
