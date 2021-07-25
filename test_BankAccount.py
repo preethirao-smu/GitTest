f78b59792fe00665952876128441fb4466a664bf
preethirao-smu <preethirao@smu.edu.sg>
2021-07-25 14:24:15 +0800

    committed for testing webhook

--- test_BankAccount.py
+++ test_BankAccount.py
@@ -12,8 +12,8 @@ from Bank_Account import BankAccount
 def test_deposit():
     account = BankAccount(124124,100)
     assert account.getAcctNum() == 124124
-    account.deposit(120)
-    assert account.getBalance() == 220
+    account.deposit(110)
+    assert account.getBalance() == 210
 
 # function to test the withdraw() func of BankAccount
 # create a BankAccount Object
