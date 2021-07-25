bd542b9d60666168c0b729c19828c2d877176efb
preethirao-smu <preethirao@smu.edu.sg>
2021-07-26 01:09:53 +0800

    test jenkins build1

--- test/test_bank_account.py
+++ test/test_bank_account.py
@@ -12,7 +12,7 @@ class testBankAccount(unittest.TestCase):
     self.assertEqual(bal, 1000)
     self.assertEqual(s.name(), "Swarna")
     #calling functions with that class
-    self.assertEqual(s.deposit(500), 1500)
+    self.assertEqual(s.deposit(550), 1550)
  def test_withdraw(self):
     #creating an object of class
     s = BankAccount(1500, "Swarna")
